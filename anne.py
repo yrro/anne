e85fffbad6d64bb284d6d495df64463d7915295d
Sam Morris <sam@robots.org.uk>
2006-09-19 00:45:29 +0100

    Grab headlines from multiple feeds; Limit flood rate to one line per two seconds

--- anne.py
+++ anne.py
@@ -1,25 +1,41 @@
 #! /usr/bin/env python
 #
 # Bot to anounce Debian Security Advisories to a channel.
-#
-# Version 20060423
 # Written by Sam Morris <sam@robots.org.uk>
+# $Id: anne.py,v 1.2 2006/05/18 00:13:01 sam Exp $
+#
+# $Log: anne.py,v $
+# Revision 1.2  2006/05/18 00:13:01  sam
+# Grab headlines from multiple feeds.
+# Limit flood rate to one line per two seconds.
+#
 
 """Bot to announce Debian Security Advisories to a channel."""
 
-# http://rss.gmane.org/messages/excerpts/gmane.linux.debian.user.news
-# http://rss.gmane.org/messages/excerpts/gmane.linux.debian.devel.announce
-# http://rss.gmane.org/messages/excerpts/gmane.linux.debian.user.announce
+version = '$Id: anne.py,v 1.2 2006/05/18 00:13:01 sam Exp $';
 
-#feed = 'http://rss.slashdot.org/Slashdot/slashdot'
-#feed = 'http://newsrss.bbc.co.uk/rss/newsonline_uk_edition/front_page/rss.xml'
-feed = 'http://www.debian.org/security/dsa'
+# Functions to print out headlines
+def getHeadlines_summary (feed, data):
+	return sets.Set (map (lambda e: '%s: %s <%s>' % (e['title'], e['summary'], e['link']), data['entries']))
+def getHeadlines (feed, data):
+	return sets.Set (map (lambda e: '%s: <%s>' % (e['title'], e['link']), data['entries']))
+
+# Feed config
+feeds = [{'name': 'debian-security-announce', 'url': 'http://www.debian.org/security/dsa', 'filter': getHeadlines_summary},
+         {'name': 'debian-news', 'url': 'http://rss.gmane.org/messages/excerpts/gmane.linux.debian.user.news', 'filter': getHeadlines},
+         {'name': 'debian-devel-announce', 'url': 'http://rss.gmane.org/messages/excerpts/gmane.linux.debian.devel.announce', 'filter': getHeadlines},
+         {'name': 'debian-announce', 'url': 'http://rss.gmane.org/messages/excerpts/gmane.linux.debian.user.announce', 'filter': getHeadlines}]
 refresh = 60 * 60 # seconds
+#feeds = [{'name': 'yahoo', 'url': 'http://rss.news.yahoo.com/rss/topstories', 'filter': getHeadlines},
+#          {'name': 'google', 'url': 'http://news.google.com/?output=rss', 'filter': getHeadlines}]
+
+# IRC config
 server = 'irc.uk.quakenet.org'
 port = 6667
 nick = 'anne'
 channel = 'debian'
 
+# Here be dragons
 from twisted.protocols import irc
 from twisted.internet import reactor, protocol
 from twisted.python import log
@@ -30,32 +46,29 @@ import sys
 import sets
 import feedparser
 
-headlines = sets.Set ()
-
-def getHeadlines (feed):
-    """
-    Returns a Set of strings that could be printed to a channel
-    """
-    return sets.Set (map (lambda e: '%s: %s <%s>' % (e['title'], e['summary'], e['link']), feed['entries']))
-
 def refreshFeeds (factory):
-	print 'updating...'
-	global headlines
-	headlines_prev = headlines
+	for feed in feeds:
+		# this holds the headlines for each feed
+		# initialize it to be the empty set
+		if not feed.has_key ('headlines'):
+			feed['headlines'] = sets.Set ()
+			
+		headlines_prev = feed['headlines']
 
-	# if there is an error, feedparser will return an empty collection
-	# if this happens, just use the previous set of headlines
-	headlines_tmp = getHeadlines (feedparser.parse (feed))
-	if len (headlines_tmp) > 0:
-		headlines = headlines_tmp
-	del headlines_tmp
+		# if there is an error, feedparser will return an empty collection
+		# if this happens, just use the previous set of headlines
+		headlines_tmp = feed['filter'] (feed, feedparser.parse (feed['url']))
+		if len (headlines_tmp) > 0:
+			feed['headlines'] = headlines_tmp
+		del headlines_tmp
 
-	headlines_new = headlines.difference (headlines_prev)
-	print '%s new headlines' % (len (headlines_new))
+		headlines_new = feed['headlines'].difference (headlines_prev)
+		print '%s: %d new headlines' % (feed['name'], len (headlines_new))
 
-	if len (headlines_prev) > 0 and len (headlines_new) == len (headlines_prev):
-		print 'might have lost some headlines'
-	map (factory.announce, headlines_new)
+		if len (headlines_prev) > 0:
+			if len (headlines_new) == len (headlines_prev):
+				print '%s: might have lost some headlines' % (feed['name'])
+			map (factory.announce, headlines_new)
 
 class AnnounceBot (irc.IRCClient):
 	'''An IRC but to announce things to a channel.'''
@@ -99,11 +112,12 @@ class AnnounceBotFactory (protocol.ReconnectingClientFactory):
 		self.bot.factory = self
 		self.bot.nickname = nick
 		self.bot.realname = 'Announce Bot'
+		self.bot.lineRate = 2
 		return self.bot
 
 	def announce (self, announcement):
 		if self.bot == None:
-			print 'not connected; skipping "%s..."' % (announcement[0:26])
+			print 'not connected; skipping "%s"' % (announcement)
 		else:
 			self.bot.msg ('#%s' % (self.channel), announcement.encode ('utf-8'))
 	
@@ -118,6 +132,7 @@ class AnnounceBotFactory (protocol.ReconnectingClientFactory):
 
 if __name__ == '__main__':
 	log.startLogging (sys.stdout)
+	print 'anne (%s)' % (version)
 
 	factory = AnnounceBotFactory (channel)
 	reactor.connectTCP (server, port, factory)
