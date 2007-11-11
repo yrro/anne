ab2e01d5e34a3dc308679d825ee512cb75cc43f4
Sam Morris <sam@robots.org.uk>
2007-11-11 01:00:43 +0000

    Add xkcd.

--- anne.py
+++ anne.py
@@ -33,7 +33,8 @@ feeds = [{'name': 'debian-security-announce',   'url': 'http://www.debian.org/se
          {'name': 'Spyblog',                    'url': 'http://p10.hostingprod.com/@spyblog.org.uk/blog/atom.xml'},
          {'name': 'Ars Technica',               'url': 'http://feeds.arstechnica.com/arstechnica/BAaf'},
          {'name': 'Groklaw News Picks',         'url': 'http://www.groklaw.net/backend/GLNewsPicks.rdf'},
-         {'name': 'Groklaw',                    'url': 'http://www.groklaw.net/backend/GrokLaw.rdf'}]
+         {'name': 'Groklaw',                    'url': 'http://www.groklaw.net/backend/GrokLaw.rdf'},
+         {'name': 'xkcd',                       'url': 'http://xkcd.com/rss.xml'}]
 
 #feeds = [{'name': 'yahoo',  'url': 'http://rss.news.yahoo.com/rss/topstories', 'headline': headlines_title},
 #         {'name': 'google', 'url': 'http://news.google.com/?output=rss', 'headline': headlines_title},
